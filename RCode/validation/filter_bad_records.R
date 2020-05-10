eab10441bf557a09cba8cff5d6b81067dc2dc1a8
cindyyawencheng <ccheng623@gmail.com>
2020-05-10 16:00:24 +0200

    correct errors noted in asana

--- RCode/validation/filter_bad_records.R
+++ RCode/validation/filter_bad_records.R
@@ -15,15 +15,50 @@
 
 qualtrics = mutate(qualtrics,
                    validation=case_when(
-                                        # RA Cheng-Hao Shen had some pretty complicated issues with Palau, that Cindy can resolve
-                                        # at a later date, but for now, lets just remove Palau from the data
-                                        init_country == 'Palau'~FALSE,
                                         # ----- Cruise ships 
                                         # lets table/remove this obs for now
                                         # record id: 933728 --; The Government of Canada has chartered a plane to repatriate Canadians on board the Diamond Princess cruise ship docked in Yokohama, Japan. The aircraft will bring passengers from Japan to Canadian Forces Base Trenton, after which they will be assessed and transported to the NAV Canada Training Institute in Cornwall, Ontario, to undergo a further 14-day period of quarantine.
                                         record_id == 933728~FALSE,
                                         # 2890593 ;All American passengers have now disembarked from the Grand Princess.  The following reflects the count as of 8:30 p.m. PDT on March 12: \n \n480 total individuals were disembarked from the ship on March 12, 2020:\n\n    218 individuals were transported via chartered flight to Marine Corps Air Station Miramar near San Diego, California\n    251 individuals were transported via charted flight to Dobbins Air Reserve base near Marietta, Georgia\n    11 individuals were transported for care.
                                         record_id == 2890593~FALSE,
+
+                                        # RA Arianna Shouten asked to delete these entries
+                                        record_id == 6724687~FALSE,
+                                        record_id == 7672988~FALSE,
+
+                                        # RA Konstanze Schönfeld asked to delete these entries
+                                        record_id == 9921492~FALSE,
+
+                                        # RA Natalia asked to delete these entries
+                                        record_id %in% c(715591,
+                                                        6882933, 
+                                                        9665468, 
+                                                        8402906,
+                                                        5881813, 
+                                                        5903938,
+                                                        4640429,
+                                                        2952930,
+                                                        4527239)~FALSE,
+
+                                        # RA Chan sau kan asked to delete this entry
+                                        record_id == 1781277~FALSE,
+
+                                        # RA Fadilah asked to delete this entry
+                                        record_id == 6810129~FALSE,
+
+                                        # RA Angela He asked to delete this entry
+                                        record_id == 6133578~FALSE,
+
+                                        # RA Sarah Edmonds asked to delete these entries
+                                        record_id %in% c(7485639, 3751423,2768097)~FALSE,
+
+                                        # remove these entries for Cara Kim
+                                            # these pertain to the #g20 issue; Cara made new entries instead of updating her overwritten corrections
+                                        record_id %in% c(2554850, 2947205, 8172521, 2473584, 5594843)~FALSE,
+
+                                        # remove this entry for Caress as part of #g20 issue
+                                        record_id %in% c(2904192, 2501721)~FALSE,
+
                                         # this entry clearly wrong, remove, validate/fix later
                                         target_who_what_10_TEXT %in% c("Tests","Facilities were meant to increase testing capacity",
                                                                        "Also foreign residents")~FALSE,
@@ -38,7 +73,7 @@ q$StartDate<-as.Date(q$StartDate)
 q<-q[which(q$date_announced >q$StartDate),]
 q<-q$record_id 
 qualtrics<- qualtrics[!qualtrics$record_id%in% q,]
-q
+
 
 #filter all, where announced data > source published date (unlogic)
 q <- qualtrics

c08554c34b6f2cf95059de7c54b579425e206f64
Luca Messerschmidt <l.messerschmidt@zeppelin-university.net>
2020-06-26 19:20:11 +0200

    update data protest population

--- RCode/simulation.R
+++ RCode/simulation.R
@@ -1,5 +1,5 @@
 #City and Protest data
-
+rm(list=ls())
 library(readr)
 library(tidyverse)
 # Import Count Love Data
@@ -9,18 +9,23 @@ count_love <- read_csv("data/simulation/protest/count_love/count_love_racial_inj
 
 census<- read_csv("data/simulation/protest/census_city_data/sub-est2019_all.csv")
 
+
+census<- census[,c(9,10,22)]
+
 #Change City Districts to Cities
 library(qdap)
 count_love$NAME<-char2end(count_love$City, ", ")
 count_love$STNAME<-count_love$`State Name`
 
+census$NAME <- gsub(" city", "", census$NAME)
 # Left Join by State and City
 
-test<-left_join(count_love,census,by=c("STNAME","NAME"))
+join<-left_join(count_love,census,by=c("STNAME","NAME"))
 
-table(census$STNAME)
-table(count_love$STNAME) 
+final<- join %>% 
+  distinct(Date, Location, Attendees, Source, .keep_all = TRUE)
 
-count_love$STNAME %in% census$STNAME 
 
-count_love$NAME %in% census$NAME 
+#save csv
+
+write.csv(final,"data/simulation/protest/protest_population.csv")
